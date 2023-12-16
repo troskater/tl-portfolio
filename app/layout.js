5a61a981a7bf1b005d47d1d72b9e318b10893713
Troy L. <troskater@gmail.com>
2023-12-16 12:54:56 -0800

    Home effects/styles polish.

--- app/layout.js
+++ app/layout.js
@@ -17,7 +17,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={font.className}>
+      <body>
         <NavMenu />
         {children}
         <Footer />
